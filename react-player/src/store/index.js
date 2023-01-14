c889fcedab0fd90cc10c25ddfec01b536c702659
Farrux Ro'zmetov <farrukhruzmetov2002@gmail.com>
2023-01-14 13:10:38 +0500

    new UI addedd

--- react-player/src/store/index.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import  musicList  from '../reducers';
-export const store = configureStore({
-  reducer: {
-    musicList,
-  },
-  middleware: [],
-  devTools: true,
-});
